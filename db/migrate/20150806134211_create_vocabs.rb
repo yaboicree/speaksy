0835566075b4005a1eca86c1077f1073d96ca4f7
cree <andersan@umich,edu>
2015-10-23 13:57:15 -0400

    remove 'vocablists' foreignkey in vocabs --- had already done 2nd migration with 'vocab_lists' references and foreignkey

--- db/migrate/20150806134211_create_vocabs.rb
+++ db/migrate/20150806134211_create_vocabs.rb
@@ -2,10 +2,8 @@ class CreateVocabs < ActiveRecord::Migration
   def change
     create_table :vocabs do |t|
       t.string :word
-      t.references :vocablist, index: true
 
       t.timestamps null: true
     end
-    add_foreign_key :vocabs, :vocablists
   end
 end
