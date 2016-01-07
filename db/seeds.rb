945723cf7bd8c0f955b09a87ea3b98ed04f01a7f
cree <andersan@umich,edu>
2016-01-07 10:08:06 -0500

    edited chapters index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@
 						 ## need to google how to create seed data with Devise... this probably wont work
 
 12.times do |n|
-Chapter.create!(name: "Chapter #{n}")
+Chapter.create!(name: "Chapter #{n}", description: "This 
+	lesson will cover basic grammar, some vocabulary about 
+	farm animals and has some cool arcade style games to help you learn.")
 end
 
 chapter = Chapter.first
@@ -26,7 +28,10 @@ Vocab.create!({vocab_list_id: 1, en: "Japan", jp: "日本"})
 
 
 ###for some reason this is creating two identical grammar points
-GrammarPoint.create!({lesson_id: 1, content: 'は is the topic marker particle. This means that は (pronounced "wa") can mark the subject of the sentence (the actor) or the subject being discussed.
+GrammarPoint.create!({lesson_id: 1, content: 'は is the 
+	topic marker particle. This means that は (pronounced "wa") 
+	can mark the subject of the sentence (the actor) or 
+	the subject being discussed.
 
 Example: 
 "I am a student." 
